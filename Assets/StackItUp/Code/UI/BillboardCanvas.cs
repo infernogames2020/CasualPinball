8c87cc1e35fc7557786c1a60350d02a5fdc60e48
FuriousPlay <furiousplay86@gmail.com>
2020-05-01 22:06:33 +0530

    Billboard world canvas.

---
+++ Assets/StackItUp/Code/UI/BillboardCanvas.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BillboardCanvas : MonoBehaviour
+{
+	private Transform cameraTransform;
+	private Quaternion originalRotation;
+    void Start()
+    {
+		originalRotation = transform.rotation;
+		cameraTransform = Camera.main.transform;
+    }
+
+    void Update()
+    {
+		transform.rotation = cameraTransform.rotation * originalRotation;
+    }
+}
